30808fac4824461470c180a82a4ea1b81bdd642c
ross <ross@beastly>
2012-11-26 01:46:28 +0800

    Actaully do some tests.

--- features/step_definitions/calculation_steps.rb
+++ features/step_definitions/calculation_steps.rb
@@ -4,12 +4,15 @@ end
 
 Then /^calculate the mean score of all the stories$/ do
   @mean = @hacker_news.mean
+  @mean.should be_a_kind_of(Fixnum)
 end
 
 Then /^calculate the mode of the score of all the stories$/ do
   @mode = @hacker_news.mode
+  @mode.should be_a_kind_of(Array)
 end
 
 Then /^calculate the median of the score of all the stories$/ do
   @median = @hacker_news.median
+  @median.should be_a_kind_of(Fixnum)
 end
